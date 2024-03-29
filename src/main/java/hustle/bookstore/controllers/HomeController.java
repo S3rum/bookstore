91c24c732f7ea4fbdd04c6a4bfd8dc030fb95e32
S3rum <tasospa2001@gmail.com>
2024-03-29 02:31:16 +0200

    v0.6.2

--- src/main/java/hustle/bookstore/controllers/HomeController.java
+++ src/main/java/hustle/bookstore/controllers/HomeController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
-    @GetMapping("/login")
-    public String showLoginForm() {
-        return "login"; // Return the name of your template
+public class HomeController {
+    @GetMapping("/home")
+    public String showHome() {
+        return "home"; // Return the name of your template
     }
 }
